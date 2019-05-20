6df4b68e62e39cebe7c54a15a3e65805eb9d9d86
Brady Mann <Football3320>
2019-05-20 14:18:30 -0400

    started redoing the translate method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,14 +7,13 @@ class Main {
     static boolean translate = true;
     public static void main(String[] args) {
     greeting();
-    translate(keyboard);
     }
 
     private static void greeting(){
         System.out.println("Hello!");
         System.out.println("Welcome to the Morse Code/English Translator!");
     }
-    public static void translate(Scanner keyboard){
+    public static String ToMorse(String message){
         String [] morseCode =
                 //A,B,C,D,E,F,G
                 {"*-","-***","-*-*","-**","*","**-*","--*",
@@ -25,9 +24,25 @@ class Main {
                         //X,Y,Z,1,2,3,4
                         "-**-","-*--","--**","*----","**---","***--","****-",
                         //5,6,7,8,9,0
-                        "*****","-****","--***","---**","----*","-----"
+                        "*****","-****","--***","---**","----*","-----"};
+        String [] chars={"a", "b", "c", "d", "e", "f", "g",
+                "h", "i", "j", "k", "l", "m", "n", "o",
+                "p", "q", "r", "s", "t", "u", "v", "w",
+                "x", "y", "z", "1", "2", "3", "4",
+                "5", "6", "7", "8", "9", "0"};
+        String [] caps={"A", "B", "C", "D", "E", "F", "G",
+                "H", "I", "J", "K", "L", "M", "N", "O",
+                "P", "Q", "R", "S", "T", "U", "V", "W",
+                "X", "Y", "Z"};
 
-                };
+        for (int a=0; a<=44; a++){
+            message=message.replace(chars[a], morseCode[a]);
+        }
+            for (int b=0; b<=44; b++){
+            message=message.replace(caps[b], morseCode[b]);
+        }
+
+        public static String ToEng (String message){
         String[] english =
                 {"A","B","C","D","E","F","G",
                         "H","I","J","K","L","M","N","O",
@@ -35,31 +50,8 @@ class Main {
                         "X","Y","Z","1","2","3","4",
                         "5","6","7","8","9","0"
                 };
-        System.out.println("Type 'morse' to do Morse Code to English");
-        System.out.println("Type 'english' to do English to Morse Code");
-        String message = keyboard.nextLine().toLowerCase();
-        String result = "21";
-        int I, J = 0;
-        if (message == "english"){
-            message = english[J];
-            result = morseCode[J];
-        }
-        else if (message == "morse"){
-            message = morseCode[J];
-            result = english[J];
-        }
-        char[] chars = message.toCharArray();
-        for (I = 0; I < chars.length; I++){
-            for (J = 0; J < chars.length; J++){
-                if (message == "english"){
-                    message = message.replace(english[I], morseCode[I]);
-                }
-                    else if (message == "morse"){
-                    message = message.replace(morseCode[I], english[I]);
-                }
-                }
-            }
-            System.out.println(message);
+
+
         }
 
     }
